f48c5c0047b4bb988e00c7faafdd0e918e43e7fd
Cohen Mejan <cohenmejan@gmail.com>
2024-12-25 00:59:03 -0500

    add more to premake files

--- premake5.lua
+++ premake5.lua
@@ -13,16 +13,30 @@ project "VST3SDK"
 	objdir "%{wks.location}/bin-obj/%{cfg.buildcfg}/%{prj.name}"
 
 	includedirs {
-		"%{wks.location}/Dependencies/VST3SDK"
+		"%{wks.location}/Dependencies/VST3SDK",
+		"%{wks.location}/Dependencies/VST3SDK/base/thread/include",
+		"%{wks.location}/Dependencies/VST3SDK/vstgui4",
 	}
 
 	files {
+		"base/source/*.h",
+		"base/source/*.cpp",
+		"base/thread/include/*.h",
+		"base/thread/include/*.cpp",
+		"base/thread/source/*.h",
+		"base/thread/source/*.cpp",
 		"pluginterfaces/base/*.h",
 		"pluginterfaces/base/*.cpp",
-		"public.sdk/source/*.h",
-		"public.sdk/source/*.cpp",
+		"pluginterfaces/gui/*.h",
+		"pluginterfaces/gui/*.cpp",
+		"pluginterfaces/vst/*.h",
+		"pluginterfaces/vst/*.cpp",
 		"public.sdk/source/common/*.h",
 		"public.sdk/source/common/*.cpp",
+		"public.sdk/source/main/*.h",
+		"public.sdk/source/main/*.cpp",
+		"public.sdk/source/vst/*.h",
+		"public.sdk/source/vst/*.cpp",
 		"public.sdk/source/vst/hosting/*.h",
 		"public.sdk/source/vst/hosting/*.cpp",
 		"public.sdk/source/vst/utility/*.h",
@@ -31,7 +45,9 @@ project "VST3SDK"
 
 	removefiles {
 		"**_linux.cpp",
-		"linux**.cpp",
+		"**linuxmain.cpp",
+		"**macmain.cpp",
+		"**vstsinglecomponenteffect**"
 	}
 
 	defines {
